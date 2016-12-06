87fedcd17c96d6616bbfcfff5d03c4965ecaf87e
Asya Dikova <asya.iv.dikova@gmail.com>
2016-12-07 01:52:41 +0200

    footer, styling

---
+++ BulgarianInfoGuide/src/components/common/Footer.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import './common-style.css';
+
+export default class Footer extends Component{
+    render(){
+        return(
+            <div className="footer">Bulgarian Info Guide, 2016</div>
+        )
+    }
+}
